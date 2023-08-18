e3d5c6da7bb7ac9d0770b9a1d7a13c68493f728a
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-18 20:40:12 +0100

    refactor: simplify sector

--- src/entities/sector.py
+++ src/entities/sector.py
@@ -1,14 +1,13 @@
 from dataclasses import dataclass
 from random import choice
 
-SECTOR_LIST = ["caves", "tombs", "sewers", "dungeons"]
 
-
-class SECTOR_WEIGHTS:
-    CAVES = (15, 4, 1)
-    TOMBS = (1, 1, 18)
-    SEWERS = (9, 3, 8)
-    DUNGEONS = (6, 7, 6)
+SECTOR_WEIGHTS = {
+    "caves": (15, 4, 1),
+    "tombs": (1, 1, 18),
+    "sewers": (9, 3, 8),
+    "dungeons": (6, 7, 6),
+}
 
 
 @dataclass
@@ -16,15 +15,8 @@ class Sector:
     name: str = ""
     current_weights: tuple = (1, 1, 1)
 
-    @staticmethod
-    def _get_weights_dict():
-        return {
-            "caves": SECTOR_WEIGHTS.CAVES,
-            "tombs": SECTOR_WEIGHTS.TOMBS,
-            "sewers": SECTOR_WEIGHTS.SEWERS,
-            "dungeons": SECTOR_WEIGHTS.DUNGEONS,
-        }
-
     def roll_sector(self):
-        self.name = choice(list(filter(lambda x: x != self.name, SECTOR_LIST)))
-        self.current_weights = self._get_weights_dict()[self.name]
+        self.name = choice(
+            list(filter(lambda x: x != self.name, SECTOR_WEIGHTS.keys()))
+        )
+        self.current_weights = SECTOR_WEIGHTS[self.name]
