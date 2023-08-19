946cce4bb1f90abb49bff2efb889a59478534b11
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-19 15:54:44 +0100

    test: add remove equipment test 3

--- src/entities/tests/test_inventory.py
+++ src/entities/tests/test_inventory.py
@@ -1,4 +1,5 @@
 from entities.inventory import PlayerInventory
+from entities.player import PlayerCharacter
 from entities.item import Item
 
 
@@ -21,8 +22,6 @@ class Test_append_to_inventory:
 class Test_remove_random_item:
     def test_removes_equipment_from_inventory(self):
         # Given
-        player_inventory = PlayerInventory()
-
         item1 = Item(
             name="item1",
             type="weapon",
@@ -33,6 +32,7 @@ class Test_remove_random_item:
             type="weapon",
         )
 
+        player_inventory = PlayerInventory()
         player_inventory.current_equipment = {
             "Item": item1,
             "Item2": item2,
@@ -55,14 +55,13 @@ class Test_remove_random_item:
 
     def test_unequips_item(self):
         # Given
-        player_inventory = PlayerInventory()
-
         item1 = Item(
             name="item1",
             type="weapon",
             is_equipped=True,
         )
 
+        player_inventory = PlayerInventory()
         player_inventory.current_equipment = {
             "Item": item1,
         }
@@ -80,3 +79,71 @@ class Test_remove_random_item:
 
         # Then
         assert not item1.is_equipped
+
+    def test_updates_player_stats_for_weapon(self):
+        # Given
+        item1 = Item(
+            name="item1",
+            type="weapon",
+            is_equipped=True,
+            dfc=1,
+            psd=2,
+            atk=3,
+            dmg=4,
+        )
+
+        player_inventory = PlayerInventory()
+        player_inventory.current_equipment = {
+            "Item": item1,
+        }
+
+        player = PlayerCharacter(
+            name="player",
+            atk=10,
+            dfc=10,
+            dmg=10,
+            psd=10,
+        )
+
+        # When
+        player_inventory.remove_random_item(player)
+
+        # Then
+        assert player.dfc == 10
+        assert player.psd == 10
+        assert player.atk == 7
+        assert player.dmg == 6
+
+    def test_updates_player_stats_for_attire(self):
+        # Given
+        item1 = Item(
+            name="item1",
+            type="attire",
+            is_equipped=True,
+            dfc=1,
+            psd=2,
+            atk=3,
+            dmg=4,
+        )
+
+        player_inventory = PlayerInventory()
+        player_inventory.current_equipment = {
+            "Item": item1,
+        }
+
+        player = PlayerCharacter(
+            name="player",
+            atk=10,
+            dfc=10,
+            dmg=10,
+            psd=10,
+        )
+
+        # When
+        player_inventory.remove_random_item(player)
+
+        # Then
+        assert player.dfc == 9
+        assert player.psd == 8
+        assert player.atk == 10
+        assert player.dmg == 10
