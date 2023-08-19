b1747773ded96e609e6b229e24a985ee5251ad39
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-19 15:50:15 +0100

    test: add remove equipment test 2

--- src/entities/tests/test_inventory.py
+++ src/entities/tests/test_inventory.py
@@ -52,3 +52,31 @@ class Test_remove_random_item:
         # Then
         assert player_inventory.current_equipment["Item1"] == None
         assert player_inventory.current_equipment["Item2"] == item2
+
+    def test_unequips_item(self):
+        # Given
+        player_inventory = PlayerInventory()
+
+        item1 = Item(
+            name="item1",
+            type="weapon",
+            is_equipped=True,
+        )
+
+        player_inventory.current_equipment = {
+            "Item": item1,
+        }
+
+        player = PlayerCharacter(
+            name="player",
+            atk=10,
+            dfc=10,
+            dmg=10,
+            psd=10,
+        )
+
+        # When
+        player_inventory.remove_random_item(player)
+
+        # Then
+        assert not item1.is_equipped
